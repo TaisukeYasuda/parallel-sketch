382b1b77fe23a740e30424e80636248bbfd18db2
Taisuke Yasuda <taisukey@andrew.cmu.edu>
2018-04-16 13:38:49 -0400

    add descriptions and organization for test directory

---
+++ test/random_matrices/gen_random_matrices.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+test_files = [
+    'test0.txt',
+    'test1.txt',
+    'test2.txt',
+    'test3.txt',
+    'test4.txt',
+    'test5.txt',
+    'test6.txt',
+    'test7.txt',
+    'test8.txt',
+    'test9.txt'
+]
+matrix_dim_rows = 500
+matrix_dim_cols = 50
+
+for test_file in test_files:
+    filename = 'med_{}'.format(test_file)
+    np.savetxt(filename, np.random.rand(matrix_dim_rows, matrix_dim_cols))
