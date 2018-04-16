a992dccab976a63fe6d0e2dbb915b8909304c44f
Taisuke Yasuda <taisukey@andrew.cmu.edu>
2018-04-16 14:14:21 -0400

    add image test cases in txt file format

---
+++ test/images/gen_images.py
@@ -0,0 +1,7 @@
+import numpy as np
+from scipy.misc import imread
+
+test_files = ['bibimbap', 'flower']
+for test_file in test_files:
+    img = imread('{}.jpg'.format(test_file), mode='L')
+    np.savetxt('{}.txt'.format(test_file), img)
