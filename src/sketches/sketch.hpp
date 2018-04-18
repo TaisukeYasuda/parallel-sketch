2601821e891e8d8c0b5b8016d40a00872b5a931b
Eliot W Robson <erobson@andrew.cmu.edu>
2018-04-18 17:29:41 -0400

    added header file

--- src/sketches/sketch.hpp
+++ src/sketches/sketch.hpp
@@ -1,7 +1,30 @@
+namespace sketch {
+//TODO case based on underlying matrix type
+//sketch will be stored internally (so each object is the sketch itself)
+//this allows for use of operator overloading
 template <typename T>
 class Sketch {
+    protected:
+        T *SA;
     public:
-        virtual void sketch(matrix *A, matrix *SA, size_t n, size_t d,
-            size_t p) = 0;
-
+        virtual void sketch(T *A) = 0;
 };
+
+template <typename T>
+class ObliviousSketch: public Sketch<T> {
+    public:
+        ObliviousSketch(size_t num_rows);
+};
+
+template <typename T>
+class AdaptiveSketch: public Sketch<T> {
+    public:
+        AdaptiveSketch();
+};
+
+template<typename T>
+class GaussianProjection : public ObliviousSketch<T> {};
+class CountSketch : public ObliviousSketch<T>  {}
+class UniformSamplingSketch : public ObliviousSketch {}
+class LeverageScoreSketch : public AdaptiveSketch {}
+}
