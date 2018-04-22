46bc3eca7425fa583cd6448e71b5281379f4a0b9
TaisukeYasuda <taisukey@andrew.cmu.edu>
2018-04-22 16:00:37 -0400

    fix mistakes in constants and add constant tracking for leverage score sampling

--- src/sketches/leverage_score_sketch.cpp
+++ src/sketches/leverage_score_sketch.cpp
@@ -7,9 +7,10 @@
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 #include "sketch.hpp"
+#include <math.h>
 #include <random>
 
-namespace sketch{
+namespace sketch {
 
 template <typename I, typename T>
 leverage_score_sketch<I, T>::leverage_score_sketch(size_t p, size_t n) {
@@ -22,6 +23,21 @@ template <typename I, typename T>
 void leverage_score_sketch<I, T>::sketch(I *A, T *SA) {
     // @TODO compute leverage scores and sample
     (*SA) = (*S) * (*A);
+}
+
+template <typename I, typename T>
+size_t leverage_score_sketch<I, T>::eps_approx_rows(
+        double eps, size_t n, size_t d) {
+    if (d <= 1) {
+        std::string info;
+        info = std::string("Too few columns. Expected at least 2 columns.");
+        throw bad_dimension(info);
+    } else {
+        double delta = 0.01;
+        double c = 1 + log(2.0 / delta) / log(d * 1.0);
+        size_t k = (int) ceil(c * 4.0 / 3.0 * d * log(d) / (eps * eps));
+        return std::min(n, k);
+    }
 }
 
 template class leverage_score_sketch<Eigen::MatrixXd, Eigen::MatrixXd >;
