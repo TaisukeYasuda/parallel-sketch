934149d0640bc5c8763c087cc16b13356c325b98
Eliot W Robson <erobson@andrew.cmu.edu>
2018-04-22 22:48:03 -0400

    Added (untested) cuda code for count sketch

--- src/cuda/sketches/count_sketch.cu
+++ src/cuda/sketches/count_sketch.cu
@@ -1,32 +1,76 @@
 
 
-#include "sketch.hpp"
+#include "sketch.h"
+#include <random>
+#include <vector>
 
 namespace sketch {
 
 template <typename I, typename T>
-count_sketch<I, T>::count_sketch(size_t p, size_t n) {
-    S = new Eigen::SparseMatrix<double>(p, n);
+count_sketch<I, T>::count_sketch(size_t p, size_t n_in) {
+    n = n_in; 
+    cudaMalloc(S, n * sizeof(int));
+
     std::random_device rd;
     seed = rd();
     std::mt19937 mt(seed);
     std::uniform_int_distribution<int> rand_row(0, p-1);
     std::uniform_int_distribution<int> rand_sign(0, 1);
-    std::vector<Eigen::Triplet<double> > entries;
+    int *temp = new int[n];
+
     for (unsigned int j = 0; j < n; j++) {
         unsigned int i = rand_row(mt);
-        entries.push_back(Eigen::Triplet<double>(i, j, rand_sign(mt) * 2 - 1));
+        temp[j] = rand_sign(mt) * 2 - 1;
     }
-    S->setFromTriplets(entries.begin(), entries.end());
+
+    cudaMemcpy(S, temp, n, cudaMemcpyHostToDevice);
+
+    delete[] temp;
 }
 
-template<
-__global__ void
-initialize_sketch()
+//Assume in_matrix is on device 
+template<typename I, typename T>
+__global__ void sketch_kernel(I *in_matrix, T *out_matrix, int *cols, int n) {
+    int row = blockIdx.x * blockDim.x + threadIdx.x,
+        col = blockIdx.y * blockDim.y + threadIdx.y;
+    
+    __shared__ int shared_cols[n];
+    __shared__ int signs[n];
+
+    if(threadIdx.x == 0 && threadIdx.y == 0) {
+        int col_start = blockIdx.y * blockDim.y;
+        for(int i = 0; i < n; i++)
+            shared_cols[i] = fabs(cols[i]) - 1;
+            
+            if(cols[i] < 0)
+                signs[i] = -1;
+            else
+                signs[i] = 1;
+    }
+
+    __syncthreads();
+
+    double res = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(shared_cols[i] == row) { //If 1 is in current row
+            res += in_matrix(row, i) * signs[i];
+        }
+    }
+
+    out_matrix(row, col) = res;
+}
 
 template <typename I, typename T>
 void count_sketch<I, T>::sketch(I *A, T *SA) {
-    (*SA) = (*S) * (*A);
+    int rows = n, cols = ;//TODO get columns of A matrix
+    dim3 blockDim(16, 16);
+    dim3 gridDim(
+        (rows + blockDim.x - 1) / blockDim.x,
+        (cols + blockDim.y - 1) / blockDim.y);
+    
+    sketch_kernel<<<gridDim, blockDim>>>(A, SA, S, n);
+
 }
 
 template <typename I, typename T>
