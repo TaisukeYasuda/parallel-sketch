c29912a8a24750b8656aa6ee20631a70d7743247
Eliot W Robson <erobson@andrew.cmu.edu>
2018-04-27 18:39:18 -0400

    Finished serial initialization for MAD sketch

--- src/cpp/applications/MAD_sketch.hpp
+++ src/cpp/applications/MAD_sketch.hpp
@@ -13,11 +13,17 @@
 
 class MAD_sketch {
     public:
-        MAD_sketch(size_t n, size_t d, size_t w,
+        MAD_sketch(size_t n, size_t d, size_t w, double mu_1, double mu_2, double mu_3,
+                std::vector<double> p_inj, std::vector<double> p_cont,
+                std::vector<double> p_abnd,
                 std::vector< std::pair< std::pair<size_t,size_t>, unsigned int> > *edge_list,
-                std::vector< std::vector<double> > *seeds)
+                std::vector< std::vector<double> > *seeds);
     private:
+        double mu_1;
+        double mu_2;
+        double mu_3;
         std::vector<size_t> *edge_starts; //Has all starts of edges in edge list
         std::vector<unsigned int> *edges; //Edge list itself
         std::vector< sketch::seq::count_min_sketch > *Ys; //Label scores
+        std::vector<double> *Ms; //M vector
 }
